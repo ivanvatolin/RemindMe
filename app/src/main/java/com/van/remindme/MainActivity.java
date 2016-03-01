f66cfaa96ac4f63b353770e551268657a30756e1
Van <van77@hotmail.ru>
2016-03-02 01:09:20 +0400

    Created base project structure

--- app/src/main/java/com/van/remindme/MainActivity.java
+++ app/src/main/java/com/van/remindme/MainActivity.java
@@ -2,14 +2,29 @@ package com.van.remindme;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        toolbarInit();
+    }
+
+    private void toolbarInit() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(R.string.app_name);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return false;
+            }
+        });
+        toolbar.inflateMenu(R.menu.menu);
     }
 }
